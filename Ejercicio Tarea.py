8988527e99edaa55320c4fdadbfc29ae80da70f2
GastonCabrer <gaston.cabrera2020@twk.cl>
2020-06-10 17:33:42 -0400

    first commit

---
+++ Ejercicio Tarea.py
@@ -0,0 +1,18 @@
+print("Operadores Arismeticos")
+# +, -, *, /, %, //, **
+print("Ingrese un numero")
+num1 = int (input())
+
+print("Ingrese otro numero")
+num2 = int(input())
+
+suma = num1 + num2
+resta = num1-num2
+multi = num1 * num2
+division = num1 / num2
+
+# print
+print("La suma es =", suma)
+print("La resta es =", resta)
+print("La Multiplicacion es =", multi)
+print("la Division es = ", division)
